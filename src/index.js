548d61591970915c47159eab7d063a9c54bd1331
Sagar Pednekar <sagarpednekar04@gmail.com>
2020-02-14 08:56:42 +0530

    Added husky and prettier

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) =>{ console.log(res); res.send({text:"Hello World"})})
+app.get('/', (req, res) =>{ console.log(res);  res.send({text:"Hello World"})})
 
 app.listen(port, () => {console.log(`Example app listening on port ${port}!`)})
