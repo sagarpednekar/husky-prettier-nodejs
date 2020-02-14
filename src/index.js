bf2933cd2019ffebfb2ebadfec6b3c07002cdb5f
Sagar Pednekar <sagarpednekar04@gmail.com>
2020-02-14 08:52:54 +0530

    Commit without prettier

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) =>{ console.log(res); res.send({text:"Hello World"})})
+
+app.listen(port, () => {console.log(`Example app listening on port ${port}!`)})
